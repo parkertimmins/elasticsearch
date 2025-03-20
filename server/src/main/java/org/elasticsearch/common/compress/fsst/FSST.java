50e4c7e2df4bed8f8407e268c5cf18182a1dc27a
Parker Timmins <parker.timmins@elastic.co>
2025-03-20 16:26:55 -0500

    add license for fsst code

--- server/src/main/java/org/elasticsearch/common/compress/fsst/FSST.java
+++ server/src/main/java/org/elasticsearch/common/compress/fsst/FSST.java
@@ -1,3 +1,31 @@
+// this software is distributed under the MIT License (http://www.opensource.org/licenses/MIT):
+//
+// Copyright 2018-2019, CWI, TU Munich, FSU Jena
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
+// merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// - The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// You can contact the authors via the FSST source repository : https://github.com/cwida/fsst
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * This file is a Java port of the original library shown in the license above.
+ * Original C++ library: https://github.com/cwida/fsst
+ *
+ * This file contains code derived from https://github.com/cwida/fsst and
+ * also includes significant additions by parkertimmins.
+ */
+
 package org.elasticsearch.common.compress.fsst;
 
 import java.io.IOException;
